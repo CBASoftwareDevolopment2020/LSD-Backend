3b3e5f449c784769c3cc49f4d025311a4a330a60
AdamSai <adamsaidane252@gmail.com>
2020-11-04 08:46:54 +0100

    Removed git history as there was no .gitignore before

---
+++ LSD-BackendTests/Service1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LSD_Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSD_Backend.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetDataUsingDataContractTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
